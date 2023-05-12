e87419be4d310553b7b6f4910ffc2c0424951022
zziyuni <86959644+zziyuni@users.noreply.github.com>
2023-05-12 16:44:30 +0900

    진동 함수 SettingVibration 작성

---
+++ lib/utils/setting_vibration.dart
@@ -0,0 +1,38 @@
+import 'package:vibration/vibration.dart';
+
+class SettingVibration {
+  static Future<void> setVibrate(String times) async {
+    switch (times) {
+      case "한 번 진동":
+        if (await Vibration.hasVibrator() ?? true) {
+          Vibration.vibrate();
+        }
+        break;
+      case "두 번 진동":
+        if (await Vibration.hasVibrator() ?? true) {
+          Vibration.vibrate();
+          await Future.delayed(const Duration(milliseconds: 1000));
+          Vibration.vibrate();
+        }
+        break;
+      case "세 번 진동":
+        if (await Vibration.hasVibrator() ?? true) {
+          Vibration.vibrate();
+          await Future.delayed(const Duration(milliseconds: 1000));
+          Vibration.vibrate();
+          await Future.delayed(const Duration(milliseconds: 1000));
+          Vibration.vibrate();
+        }
+        break;
+      case "연속 진동":
+      int n = 10;
+        if (await Vibration.hasVibrator() ?? true) {
+          while (n>0) {
+            Vibration.vibrate();
+            await Future.delayed(const Duration(milliseconds: 700));
+            n--;
+        }}
+        break;
+    }
+  }
+}
