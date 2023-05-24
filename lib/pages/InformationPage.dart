1ea845b25a35242b0008cdd24b3c56ba17eb2c2f
zziyuni <86959644+zziyuni@users.noreply.github.com>
2023-05-24 13:48:06 +0900

    SingleChildScrollView 추가 (스크롤 가능하게 만들기)

--- lib/pages/InformationPage.dart
+++ lib/pages/InformationPage.dart
@@ -11,37 +11,39 @@ class informationPage extends StatelessWidget{
      var w = MediaQuery.of(context).size.width;
     var h = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-            alignment: Alignment.centerLeft,
-            margin: EdgeInsets.fromLTRB(15, 35, 0, 0),
-            child: IconButton(
-              onPressed: () {
-                Get.toNamed('/');
-              },
-              icon: Image(image: AssetImage('images/icons/back.png')),
+        body: SingleChildScrollView(
+          child: Column(
+              children: [
+          Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.fromLTRB(15, 35, 0, 0),
+              child: IconButton(
+                onPressed: () {
+                  Get.toNamed('/');
+                },
+                icon: Image(image: AssetImage('images/icons/back.png')),
+              ),
+              height: 40),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                  alignment: Alignment.topLeft,
+                  margin: EdgeInsets.only(left: 10),
+                  child: Image(
+                      image: AssetImage('images/logo.png'), width: w * 0.3)),
+              Text(
+                "about AVO",
+                style: TextStyle(fontSize: 20),
+              )
+            ],
+          ),
+          Image(image: (AssetImage('images/avo_4.png')), width: w * 0.9),
+           Image(image: (AssetImage('images/avoInfo.png')), width: w * 0.9),
+        
+              ],
             ),
-            height: 40),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-                alignment: Alignment.topLeft,
-                margin: EdgeInsets.only(left: 10),
-                child: Image(
-                    image: AssetImage('images/logo.png'), width: w * 0.3)),
-            Text(
-              "about AVO",
-              style: TextStyle(fontSize: 20),
-            )
-          ],
-        ),
-        Image(image: (AssetImage('images/avo_4.png')), width: w * 0.9),
-         Image(image: (AssetImage('images/avoInfo.png')), width: w * 0.9),
-
-      ],
-    ));
+        ));
   }
 
  
--- lib/pages/SettingPage.dart
+++ lib/pages/SettingPage.dart
@@ -13,60 +13,62 @@ class settingPage extends StatelessWidget {
     var w = MediaQuery.of(context).size.width;
     var h = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-            alignment: Alignment.centerLeft,
-            margin: EdgeInsets.fromLTRB(15, 35, 0, 0),
-            child: IconButton(
-              onPressed: () {
-                Get.toNamed('/');
-              },
-              icon: Image(image: AssetImage('images/icons/back.png')),
-            ),
-            height: 40),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-                alignment: Alignment.topLeft,
-                margin: EdgeInsets.only(left: 10),
-                child: Image(
-                    image: AssetImage('images/logo.png'), width: w * 0.3)),
-            Text(
-              "알림 설정",
-              style: TextStyle(fontSize: 20),
-            )
-          ],
-        ),
-        Image(image: (AssetImage('images/avo_4.png')), width: w * 0.9),
-        Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Color(0xffBAD7B3),
-            ),
-            width: w * 0.9,
-            height: h * 0.55,
-            child: Column(
+        body: SingleChildScrollView(
+          child: Column(
               children: [
-                SizedBox(height: 20),
-                dropdownTitle("아이가 울어요"),
-                Obx(
-                  ()=> DropdownButtonWidget( selectedValue: controller.crySelected.value,
-                            onChanged: (value) => controller.updateCrySelected(value!),),
-                ),
-                dropdownTitle("아이가 불러요"),
-                Obx
-                (()=>DropdownButtonWidget(selectedValue:controller.callSelected.value,
-                            onChanged: (value) => controller.updateCallSelected(value!),),
-                ),
-                dropdownTitle("아이가 소리질러요"),
-                 Obx(() =>   DropdownButtonWidget(selectedValue: controller.shoutSelected.value,
-            onChanged: (value) => controller.updateShoutSelected(value!),)),
+          Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.fromLTRB(15, 35, 0, 0),
+              child: IconButton(
+                onPressed: () {
+                  Get.toNamed('/');
+                },
+                icon: Image(image: AssetImage('images/icons/back.png')),
+              ),
+              height: 40),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                  alignment: Alignment.topLeft,
+                  margin: EdgeInsets.only(left: 10),
+                  child: Image(
+                      image: AssetImage('images/logo.png'), width: w * 0.3)),
+              Text(
+                "알림 설정",
+                style: TextStyle(fontSize: 20),
+              )
+            ],
+          ),
+          Image(image: (AssetImage('images/avo_4.png')), width: w * 0.9),
+          Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color(0xffBAD7B3),
+              ),
+              width: w * 0.9,
+              height: h * 0.6,
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  dropdownTitle("아이가 울어요"),
+                  Obx(
+                    ()=> DropdownButtonWidget( selectedValue: controller.crySelected.value,
+                              onChanged: (value) =>controller.updateCrySelected(value!))
+                  ),
+                  dropdownTitle("아이가 불러요"),
+                  Obx
+                  (()=>DropdownButtonWidget(selectedValue:controller.callSelected.value,
+                              onChanged: (value)=>controller.updateCallSelected(value!)),
+                  ),
+                  dropdownTitle("아이가 소리질러요"),
+                   Obx(() =>   DropdownButtonWidget(selectedValue: controller.shoutSelected.value,
+              onChanged: (value) =>controller.updateShoutSelected(value!))),
+                ],
+              ))
               ],
-            ))
-      ],
-    ));
+            ),
+        ));
   }
 
   Container dropdownTitle(String title) {
