d1fd62b181664cddf5ef807ba1f2051ab0ebe527
zziyuni <86959644+zziyuni@users.noreply.github.com>
2023-05-24 13:51:06 +0900

    shared_preferences 사용 코드 추가

--- lib/controller/DropdownButtonController.dart
+++ lib/controller/DropdownButtonController.dart
@@ -1,20 +1,27 @@
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
-class DropdownButtonController extends GetxController {
+class DropdownButtonController extends GetxService {
   final RxString crySelected = RxString('한 번 진동');
   final RxString callSelected = RxString('한 번 진동');
   final RxString shoutSelected = RxString('한 번 진동');
 
-  void updateCrySelected(String value) {
+  void updateCrySelected(String value) async{
     crySelected(value);
+ final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("crying", value);
   }
 
-  void updateCallSelected(String value) {
+  void updateCallSelected(String value) async{
     callSelected(value);
+     final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("calling", value);
   }
 
-  void updateShoutSelected(String value) {
+  void updateShoutSelected(String value) async{
     shoutSelected(value);
+       final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("shouting", value);
   }
 }
