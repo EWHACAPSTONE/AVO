2eec2e1a0c8f95d1e000db52dd1659d1877a7f25
zziyuni <86959644+zziyuni@users.noreply.github.com>
2023-05-25 10:12:13 +0900

    firestore에 저장된 값으로dropdownbutton 초기값 설정

--- lib/controller/DropdownButtonController.dart
+++ lib/controller/DropdownButtonController.dart
@@ -1,27 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 
 class DropdownButtonController extends GetxService {
-  final RxString crySelected = RxString('한 번 진동');
-  final RxString callSelected = RxString('한 번 진동');
-  final RxString shoutSelected = RxString('한 번 진동');
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  void updateCrySelected(String value) async{
+  final RxString crySelected = RxString('');
+  final RxString callSelected = RxString('');
+  final RxString shoutSelected = RxString('');
+
+  DropdownButtonController() {
+    getCrySelectedValue().then((value) {
+      crySelected.value = value;
+    });
+    getShoutSelectedValue().then((value) {
+      shoutSelected.value = value;
+    });
+    getCallSelectedValue().then((value) {
+      callSelected.value = value;
+    });
+  }
+
+  void updateCrySelected(String value) async {
     crySelected(value);
- final prefs = await SharedPreferences.getInstance();
-    await prefs.setString("crying", value);
+    firestore
+        .collection("alarm")
+        .doc('3a1YFfhx4HPeTdPvAXjJ')
+        .update({"crying": value});
   }
 
-  void updateCallSelected(String value) async{
+  void updateCallSelected(String value) async {
     callSelected(value);
-     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString("calling", value);
+    firestore
+        .collection("alarm")
+        .doc('3a1YFfhx4HPeTdPvAXjJ')
+        .update({"calling": value});
   }
 
-  void updateShoutSelected(String value) async{
+  void updateShoutSelected(String value) async {
     shoutSelected(value);
-       final prefs = await SharedPreferences.getInstance();
-    await prefs.setString("shouting", value);
+    firestore
+        .collection("alarm")
+        .doc('3a1YFfhx4HPeTdPvAXjJ')
+        .update({"shouting": value});
+  }
+
+  Future<String> getCrySelectedValue() async {
+    DocumentSnapshot snapshot =
+        await firestore.collection("alarm").doc('3a1YFfhx4HPeTdPvAXjJ').get();
+    Map<String, dynamic>? data = snapshot.data() as Map<String, dynamic>?;
+    String cryValue = data?['crying'] ?? '한 번 진동';
+    return cryValue;
+  }
+
+  Future<String> getCallSelectedValue() async {
+    DocumentSnapshot snapshot =
+        await firestore.collection("alarm").doc('3a1YFfhx4HPeTdPvAXjJ').get();
+    Map<String, dynamic>? data = snapshot.data() as Map<String, dynamic>?;
+    String callValue = data?['calling'] ?? '한 번 진동';
+    return callValue;
+  }
+
+  Future<String> getShoutSelectedValue() async {
+    DocumentSnapshot snapshot =
+        await firestore.collection("alarm").doc('3a1YFfhx4HPeTdPvAXjJ').get();
+    Map<String, dynamic>? data = snapshot.data() as Map<String, dynamic>?;
+    String shoutValue = data?['shouting'] ?? '한 번 진동';
+    return shoutValue;
   }
 }
